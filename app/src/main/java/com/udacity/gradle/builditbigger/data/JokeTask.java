74a9ecfd29a96e1ad9028fed2b8c26bcc9b61e41
Tyler Caudell <caudell.tyler@gmail.com>
2017-06-13 11:32:40 -0500

    Updated task url address to the PCs IP and now pulls string on an emulator.

--- app/src/main/java/com/udacity/gradle/builditbigger/data/JokeTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/data/JokeTask.java
@@ -57,7 +57,7 @@ public class JokeTask extends AsyncTask<String, Void, String> {
             // Builder method call types > HttpTransport, JsonFactory, HttpRequestInitializer
             MyApi.Builder builder =
                     new MyApi.Builder(AndroidHttp.newCompatibleTransport(), AndroidJsonFactory.getDefaultInstance(), null)
-                            .setRootUrl("http://localhost:8080/_ah/api/")
+                            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                             .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                                 @Override
                                 public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
